ae8adca9f90584d9a143669595b842abdc9ea24c
Shah Jalal <rjshahjalal132@gmail.com>
2025-12-25 17:30:40 +0600

    build

---
+++ public/build/assets/app-layout-CEUxLHJV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Do_xaZHl.js";import{A as s}from"./app-sidebar-layout-lkKhZVp6.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
