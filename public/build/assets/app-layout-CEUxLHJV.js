a80044bf875ce24793350470c1abaac29aad4798
Shah Jalal <rjshahjalal132@gmail.com>
2025-12-28 11:47:45 +0600

    build

--- public/build/assets/app-layout-CEUxLHJV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-Do_xaZHl.js";import{A as s}from"./app-sidebar-layout-lkKhZVp6.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/app-layout-Z1p3mHi-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-GGrxEeiw.js";import{A as s}from"./app-sidebar-layout-DV7Hcceq.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
